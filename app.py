f6366b0fc9b3436a4b91695c2b243c5887d26597
Jordan Harris <jordan.lee.harris@icloud.com>
2022-12-02 09:22:08 +0000

    changed base landing page to correct file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def LandingPage():
-    return render_template('LandingPage.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
