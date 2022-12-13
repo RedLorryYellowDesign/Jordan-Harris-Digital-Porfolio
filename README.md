32c21d3f393510f914766b69e6de12c22f31e003
Jordan Harris <jordan.lee.harris@icloud.com>
2022-12-13 12:40:08 +0000

    Ajsuted

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a Porfolio website Build using Flask and HTML/CSS
+This is my Porfolio website Build using Flask and HTML/CSS. It acted as a tool for me to lean and test new web Dev Tools as well as showcase my skills
--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
+from Content import *
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
+def index():
+    return render_template('index.html', data1=Profile, data2=Skills)
+
+@app.route('/Design Porfolio')
 def LandingPage():
-    return render_template('index.html')
+    return render_template('Portfolio.html')
+
+@app.route('/test')
+def test():
+    #return "Hello World";
+
+    return render_template('hello_world.html', data = data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
