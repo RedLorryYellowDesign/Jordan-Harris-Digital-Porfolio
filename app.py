4035b58a693854de7132e09a72c073db234a1b05
Jordan Harris <jordan.lee.harris@icloud.com>
2022-12-15 14:27:41 +0000

    Adding Lottie Nativly and cleaning SS files

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
-from Content import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', data1=Profile, data2=Skills)
-
-@app.route('/Design Porfolio')
-def LandingPage():
-    return render_template('Portfolio.html')
-
-@app.route('/cv')
-def cv():
-    return render_template('cv.html')
-
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
