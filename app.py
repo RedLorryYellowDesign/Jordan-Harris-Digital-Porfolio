e58039f44140d731e489ff3a830fcec5aa0fbeb5
Jordan Harris <jordan.lee.harris@icloud.com>
2022-12-27 19:03:24 +0000

    lottie file changed

--- app.py
+++ app.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about-me')
+def about_me():
+    return render_template('about-me.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects')
+
 if __name__ == '__main__':
     app.run(debug=True)
