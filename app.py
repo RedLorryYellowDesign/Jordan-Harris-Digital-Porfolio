abc817d252e23451678b07416f26d00b4dc92dee
Jordan Harris <jordan.lee.harris@icloud.com>
2023-01-07 02:29:10 +0000

    Restructure of site to use Flask and Bootstrap

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
+# from flask_assets import Environment, Bundle
+from flask_caching import Cache
+
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
 
 app = Flask(__name__)
+
+app.config.from_mapping(config)
+cache = Cache(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/about-me')
-def about_me():
-    return render_template('about-me.html')
-
-@app.route('/projects')
-def projects():
-    return render_template('projects')
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
