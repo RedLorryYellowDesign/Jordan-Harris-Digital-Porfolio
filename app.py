b66fb2449fd82596e2b16828aca7859042cbdc8d
Jordan Harris <jordan.lee.harris@icloud.com>
2023-01-12 11:28:01 +0000

    Added Compression to Flask

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+# Imports
+from flask import Flask, render_template, request, redirect, url_for # Imports base flask dependantsies
 # from flask_assets import Environment, Bundle
-from flask_caching import Cache
+from flask_caching import Cache # Allows for a Cache
+from flask_compress import Compress # Allows for asset Compresion
 
+# Flast Configuration
 config = {
     "DEBUG": True,          # some Flask specific configs
     "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
@@ -9,10 +12,17 @@ config = {
 }
 
 app = Flask(__name__)
-
 app.config.from_mapping(config)
 cache = Cache(app)
+compress = Compress()
 
+# Compression of assets to improve performance
+def start_app():
+    app = Flask(__name__)
+    compress.init_app(app)
+    return app
+
+# API Call to root directory
 @app.route('/')
 def index():
     return render_template('index.html')
