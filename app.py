487cb80ac803f75164b925af83e377bff58ae777
Jordan Harris <jordan.lee.harris@icloud.com>
2023-02-15 15:25:18 +0000

    Fixing git issue

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for,jsonify
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 # from flask_assets import Environment, Bundle
 from flask_caching import Cache
 
@@ -11,13 +11,13 @@ config = {
 
 app = Flask(__name__)
 
-# the toolbar is only enabled in debug mode:
-app.debug = True
+# # the toolbar is only enabled in debug mode:
+# app.debug = True
 
-# set a 'SECRET_KEY' to enable the Flask session cookies
-app.config['SECRET_KEY'] = '<replace with a secret key>'
+# # set a 'SECRET_KEY' to enable the Flask session cookies
+# app.config['SECRET_KEY'] = '<replace with a secret key>'
 
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
 app.config.from_mapping(config)
 cache = Cache(app)
@@ -26,9 +26,9 @@ cache = Cache(app)
 def index():
     return render_template('index.html')
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return render_template('page_not_found.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=False)
