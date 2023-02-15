c6da88f63ec340bb15ff3ef8b36ee7f25a61b84f
Jordan Harris <jordan.lee.harris@icloud.com>
2023-02-15 17:56:25 +0000

    please work

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # docker-flask-gunicorn-nginx
 
 Boilerplate project template for a generic dockerized Flask application deployed with gunicorn and Nginx configurations.
-## Quickstart
-Run in this path:
-```sh
-$ docker-compose up
-```
-This will build the docker image and the production-ready Flask app will be running on http://my-public-ip:5001 (or http://localhost:5001 for local machine). 
-Static files will be served by Nginx on http://my-public-ip/static/. 
 
+## Quickstart
+
+Run in this path:
+
+```sh
+docker-compose up
+
+```
+
+This will build the docker image and the production-ready Flask app will be running on http://my-public-ip:5001 (or http://localhost:5001 for local machine).
+Static files will be served by Nginx on http://my-public-ip/static/.
--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, redirect, url_for,jsonify
+# from flask_debugtoolbar import DebugToolbarExtension
+# from flask_assets import Environment, Bundle
+from flask_caching import Cache
+
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
 
 app = Flask(__name__)
 
+# # the toolbar is only enabled in debug mode:
+# app.debug = True
+
+# # set a 'SECRET_KEY' to enable the Flask session cookies
+# app.config['SECRET_KEY'] = '<replace with a secret key>'
+
+# toolbar = DebugToolbarExtension(app)
+
+app.config.from_mapping(config)
+cache = Cache(app)
+
 @app.route('/')
-def hello_world():
-    response = {
-        "status_code":200,
-        "data":"Hello World"
-    }
-    return jsonify(response)
+def index():
+    return render_template('index.html')
+
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return render_template('page_not_found.html'), 404
+
+# if __name__ == '__main__':
+#     app.run(debug=False)
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
