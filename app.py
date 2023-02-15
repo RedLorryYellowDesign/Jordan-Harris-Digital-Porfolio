a6a8f5b67539ab187b978aa1cce882db285abe01
Jordan Harris <jordan.lee.harris@icloud.com>
2023-02-15 18:34:54 +0000

    Revising code to fix deploy issue

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for,jsonify
+from flask import Flask, render_template
+# request, redirect, url_for,jsonify
 # from flask_debugtoolbar import DebugToolbarExtension
 # from flask_assets import Environment, Bundle
-from flask_caching import Cache
+# from flask_caching import Cache
 
-config = {
-    "DEBUG": True,          # some Flask specific configs
-    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 300
-}
-
+# config = {
+#     "DEBUG": True,          # some Flask specific configs
+#     "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+#     "CACHE_DEFAULT_TIMEOUT": 300
+# }
 app = Flask(__name__)
 
 # # the toolbar is only enabled in debug mode:
